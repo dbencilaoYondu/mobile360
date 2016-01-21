b37b3c083b614e98eb05f5b4d8156b98482d21a0
Don Bencilao <Don Bencilao@egg.ph>
2016-01-21 08:23:04 +0800

    core file

---
+++ grid-menu-theme/www/js/controllers.js
@@ -0,0 +1,87 @@
+angular.module('starter.controllers', [])
+
+.controller('HeaderCtrl',function($scope,Pages){$scope.data = Pages;})
+.controller('SettingsCtrl',function($scope,$ionicModal, $ionicHistory){
+
+     $ionicModal.fromTemplateUrl('settings.html', {
+      id: '1', // We need to use and ID to identify the modal that is firing the event!
+      scope: $scope,
+      backdropClickToClose: false,
+      animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.oModalSettings = modal;
+      });
+
+      $scope.openModal = function(index) {
+        $scope.oModalSettings.show();
+      };
+
+      $scope.closeModal = function(index) {
+        $scope.oModalSettings.hide();
+      };
+
+       $scope.myGoBack = function() {
+          $ionicHistory.goBack();
+        };
+
+})
+.controller('MenuCtrl', function($scope,Pages) {
+
+      $scope.data = Pages;
+       console.log($scope);
+})
+
+.controller('AboutCtrl', function($scope,$ionicModal,Pages) {
+
+      $scope.data = Pages;
+
+
+      $ionicModal.fromTemplateUrl('mission.html', {
+      id: '1', // We need to use and ID to identify the modal that is firing the event!
+      scope: $scope,
+      backdropClickToClose: false,
+      animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.oModal1 = modal;
+      });
+
+      // Modal 2
+      $ionicModal.fromTemplateUrl('vision.html', {
+        id: '2', // We need to use and ID to identify the modal that is firing the event!
+        scope: $scope,
+        backdropClickToClose: false,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.oModal2 = modal;
+      });
+
+      $scope.openModal = function(index) {
+        if (index == 1) $scope.oModal1.show();
+        else $scope.oModal2.show();
+      };
+
+      $scope.closeModal = function(index) {
+        if (index == 1) $scope.oModal1.hide();
+        else $scope.oModal2.hide();
+      };
+
+
+       console.log($scope);
+})
+.controller('ContactCtrl', function($scope,Pages) {$scope.data = Pages;console.log($scope);})
+.controller('InquireCtrl', function($scope,Pages) {$scope.data = Pages;console.log($scope);})
+.controller('GalleryCtrl', function($scope,$stateParams, Pages) {
+  $scope.data = Pages;
+  $scope.paramsId = $stateParams.paramsId;
+  $scope.id = $stateParams.id;
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
