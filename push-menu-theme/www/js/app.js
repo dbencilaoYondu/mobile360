411c8c5ee34cf4a419efadf1dc89de5fa9fd7ae4
Don Bencilao <Don Bencilao@egg.ph>
2016-01-22 15:29:42 +0800

    add push menu

---
+++ push-menu-theme/www/js/app.js
@@ -0,0 +1,98 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers','starter.services','uiGmapgoogle-maps'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'MenuCtrl'
+  })
+  .state('app.about', {
+      url: '/about',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/about.html',
+          controller:'AboutCtrl'
+        }
+      }
+    })
+   .state('app.contact', {
+      url: '/contact',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/contact.html',
+          controller:'ContactCtrl'
+        }
+      }
+    })
+   .state('app.inquire', {
+      url : '/inquire',
+      views : {
+        'menuContent' : {
+          templateUrl : 'templates/inquire.html',
+          controller:'InquireCtrl'
+        }
+      }
+   })
+
+   //gallery route
+   .state('app.gallery', {
+      url : '/gallery',
+      views : {
+        'menuContent' : {
+          templateUrl : 'templates/gallery.html',
+          controller:'GalleryCtrl'
+        }
+      }
+   }).state('app.gallery.albums', {
+      url : '/:albumId',
+      templateUrl : 'templates/photoCollection.html',
+      controller:'GalleryCtrl'
+   }).state('app.gallery.albums.photo', {
+      url : '/:paramsId/:id',
+      views : {
+        'menuContent' : {
+          templateUrl : 'templates/gallery.html',
+          controller:'GalleryCtrl'
+        }
+      }
+   })
+
+  .state('app.map', {
+      url : '/map',
+      views : {
+        'menuContent' : {
+          templateUrl : 'templates/map.html',
+          controller:'MapCtrl'
+        }
+      }
+   })
+   ;
+   
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/about');
+});
---
+++ push-menu-theme/www/js/controllers.js
@@ -0,0 +1,140 @@
+angular.module('starter.controllers', [])
+
+
+.directive('compile', ['$compile', function ($compile) {
+  return function(scope, element, attrs) {
+    scope.$watch(
+      function(scope) {
+        return scope.$eval(attrs.compile);
+      },
+      function(value) {
+        element.html(value);
+        $compile(element.contents())(scope);
+      }
+   )};
+}])
+
+
+.controller('HeaderCtrl',function($scope,Pages){$scope.data = Pages;})
+.controller('SettingsCtrl',function($scope,$ionicModal, $ionicHistory){
+
+     $ionicModal.fromTemplateUrl('settings.html', {
+      id: '1', // We need to use and ID to identify the modal that is firing the event!
+      scope: $scope,
+      backdropClickToClose: false,
+      animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.oModalSettings = modal;
+      });
+
+      $scope.openModal = function(index) {
+        $scope.oModalSettings.show();
+      };
+
+      $scope.closeModal = function(index) {
+        $scope.oModalSettings.hide();
+      };
+
+       $scope.myGoBack = function() {
+          $ionicHistory.goBack();
+        };
+
+})
+.controller('MenuCtrl', function($scope,Pages) {
+
+      $scope.data = Pages;
+       console.log($scope);
+})
+
+.controller('AboutCtrl', function($scope,$ionicModal,Pages) {
+
+      $scope.data = Pages;
+
+
+      $ionicModal.fromTemplateUrl('mission.html', {
+      id: '1', // We need to use and ID to identify the modal that is firing the event!
+      scope: $scope,
+      backdropClickToClose: false,
+      animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.oModal1 = modal;
+      });
+
+      // Modal 2
+      $ionicModal.fromTemplateUrl('vision.html', {
+        id: '2', // We need to use and ID to identify the modal that is firing the event!
+        scope: $scope,
+        backdropClickToClose: false,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.oModal2 = modal;
+      });
+
+      $scope.openModal = function(index) {
+        if (index == 1) $scope.oModal1.show();
+        else $scope.oModal2.show();
+      };
+
+      $scope.closeModal = function(index) {
+        if (index == 1) $scope.oModal1.hide();
+        else $scope.oModal2.hide();
+      };
+
+
+       console.log($scope);
+})
+.controller('ContactCtrl', function($scope,Pages) {$scope.data = Pages;console.log($scope);})
+.controller('MapCtrl', function($scope,Pages) {$scope.data = Pages;console.log($scope);})
+.controller('InquireCtrl', function($scope,Pages) {$scope.data = Pages;console.log($scope);})
+.controller('GalleryCtrl', function($scope,$stateParams, Pages) {
+  $scope.data = Pages;
+  $scope.albumId = $stateParams.albumId;
+  $scope.id = $stateParams.id;
+  console.log($scope);
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+.controller('MapCtrl', function($scope,$interval,$log, Pages,$timeout) {
+  
+  $scope.map = Pages.data.data.location;
+  
+
+  
+  $scope.options = {
+            scrollwheel: true
+        };
+  $scope.coordsUpdates = 0;
+  $scope.dynamicMoveCtr = 0;
+  $scope.marker = {
+            id: 0,
+            options: {
+                draggable: false
+            },
+            events: {
+                dragend: function(marker, eventName, args) {
+                    var lat = marker.getPosition().lat();
+                    var lon = marker.getPosition().lng();
+                    $log.log(lat);
+                    $log.log(lon);
+
+                    $scope.marker.options = {
+                        draggable: true,
+                        labelContent: "",
+                        labelAnchor: "100 0",
+                        labelClass: "marker-labels"
+                    };
+                }
+            }
+  };
+
+console.log($scope);
+});
+
