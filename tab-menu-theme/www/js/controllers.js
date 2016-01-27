746f52a441e7d2db10ad24ceefc437848d2ade41
Don Bencilao <Don Bencilao@egg.ph>
2016-01-27 19:48:40 +0800

    menu tab

--- tab-menu-theme/www/js/controllers.js
+++ tab-menu-theme/www/js/controllers.js
@@ -1,22 +1,9 @@
 angular.module('starter.controllers', [])
 
-
-.directive('compile', ['$compile', function ($compile) {
-  return function(scope, element, attrs) {
-    scope.$watch(
-      function(scope) {
-        return scope.$eval(attrs.compile);
-      },
-      function(value) {
-        element.html(value);
-        $compile(element.contents())(scope);
-      }
-   )};
-}])
-
-
 .controller('HeaderCtrl',function($scope,Pages){$scope.data = Pages;})
-.controller('SettingsCtrl',function($scope,$ionicModal, $ionicHistory){
+.controller('SettingsCtrl',function($scope,$ionicModal, $ionicHistory,Pages){
+
+      $scope.data = Pages;
 
      $ionicModal.fromTemplateUrl('settings.html', {
       id: '1', // We need to use and ID to identify the modal that is firing the event!
@@ -40,10 +27,41 @@ angular.module('starter.controllers', [])
         };
 
 })
-.controller('MenuCtrl', function($scope,Pages) {
+.controller('MenuCtrl', function($scope,$ionicModal, Pages) {
 
       $scope.data = Pages;
-       console.log($scope);
+      console.log(Pages.data.data.pages.length);
+      if(Pages.data.data.pages.length > 4){
+        $scope.quantity = 3;
+      }else{
+        $scope.quantity = 4;
+      }
+      
+      //console.log($scope);
+
+
+     $ionicModal.fromTemplateUrl('moreMenu.html', {
+      id: '1', // We need to use and ID to identify the modal that is firing the event!
+      scope: $scope,
+      backdropClickToClose: true,
+      animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.oModalSettings = modal;
+      });
+
+      //Cleanup the modal when we're done with it!
+      $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+      });
+
+      $scope.openModal = function(index) {
+        $scope.oModalSettings.show();
+      };
+
+      $scope.closeModal = function(index) {
+        $scope.oModalSettings.hide();
+      };
+
 })
 
 .controller('AboutCtrl', function($scope,$ionicModal,Pages) {
@@ -81,20 +99,20 @@ angular.module('starter.controllers', [])
       };
 
 
-       console.log($scope);
+      // console.log($scope);
 })
-.controller('ContactCtrl', function($scope,Pages) {$scope.data = Pages; Pages.getSpecs();console.log($scope);})
-.controller('MapCtrl', function($scope,Pages) {$scope.data = Pages;console.log($scope);})
-.controller('InquireCtrl', function($scope,Pages) {$scope.data = Pages;Pages.getSpecs();console.log($scope);})
+.controller('ContactCtrl', function($scope,Pages) {$scope.data = Pages; Pages.getSpecs();})
+.controller('MapCtrl', function($scope,Pages) {$scope.data = Pages;})
+.controller('InquireCtrl', function($scope,Pages) {$scope.data = Pages;Pages.getSpecs();})
 .controller('GalleryCtrl', function($scope,  $http,$stateParams, $state,Pages,$ionicHistory) {
   $scope.data = Pages;
   $scope.albumId = $stateParams.albumId;
   $scope.id = $stateParams.id;
   $scope.$state = $state;
   Pages.getSpecs();
-  console.log($scope.albumId);
-  console.log($scope.id);
-  console.log($scope);
+  // console.log($scope.albumId);
+  // console.log($scope.id);
+  // console.log($scope);
 
    $scope.myGoBack = function() {
     window.history.back();
@@ -141,6 +159,5 @@ angular.module('starter.controllers', [])
             }
   };
 
-console.log($scope);
+//console.log($scope);
 });
-
