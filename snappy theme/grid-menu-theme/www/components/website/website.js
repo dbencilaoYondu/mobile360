ce36e1e5ebfce069d99c8b07e6f80f9540fce275
Don Bencilao <Don Bencilao@egg.ph>
2016-03-11 18:05:05 +0800

    website revised

--- snappy theme/grid-menu-theme/www/components/website/website.js
+++ snappy theme/grid-menu-theme/www/components/website/website.js
@@ -1,18 +1,24 @@
 app.controller('WebsiteCtrl', function($scope,Pages,$state,$sce) {
   $scope.data = Pages;
-  console.log('contact ctrl' );
-  console.log($scope);
-
+  
   $scope.$sce = $sce;
   //data sharing
     $scope.currentData = $state.current.data;
-    //set data to parent contact pages
-    $scope.currentWebsiteData = $scope.data.scrum2[$scope.currentData];
-    $scope.currentWebsiteDataURL = $sce.trustAsResourceUrl($scope.data.scrum2[$scope.currentData].url);
-    //transfer data to sub contact pages
-    if($scope.$parent.currentParentOfSubInfo){
-      $scope.currentContactData = $scope.$parent.currentParentOfSubInfo;
+
+    if($scope.data.scrum2[$scope.currentData]){
+       //set data to parent contact pages
+      $scope.currentWebsiteData    = $scope.data.scrum2[$scope.currentData];
+      $scope.currentWebsiteDataURL = $sce.trustAsResourceUrl($scope.currentWebsiteData.url);
     }
+    if($scope.currentParentOfSubInfo){
+       $scope.subWebsiteData    = $scope.currentParentOfSubInfo;
+      $scope.subWebsiteDataURL = $sce.trustAsResourceUrl($scope.$parent.currentParentOfSubInfo.url);
+    }
+
+    //transfer data to sub contact pages
+    console.log('contact ctrl' );
+    console.log($scope.currentParentOfSubInfo);
+    console.log($scope);
   //end of data sharing
-    console.log($scope.$parent.currentParentOfSubInfo);
+
 });
